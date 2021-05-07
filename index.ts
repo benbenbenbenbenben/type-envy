82de884e0d6661bb1a7b51d78165a59e31bb8941
Ben Babik <work@benjaminbabik.com>
2021-05-07 01:39:37 +0100

    checkpoint

--- index.ts
+++ index.ts
@@ -1,3 +1,104 @@
 
-import { env } from "./env"
 
+export const Url = "Url" as const
+export const IpAddress = "IpAddress" as const
+
+export type RequiresType<T> = {
+    [P in keyof T]
+    : T[P] extends (...args: any) => any
+    ? ReturnType<T[P]>
+    : T[P] extends readonly (infer U)[]
+    ? (
+        U extends StringConstructor
+        ? string
+        : U extends BooleanConstructor
+        ? boolean
+        : U extends NumberConstructor
+        ? number
+        : U
+    )
+    : T[P] extends typeof Url
+    ? URL
+    : T[P] extends typeof IpAddress
+    ? `${number}.${number}.${number}.${number}`
+    : never
+}
+
+export function Requires<Variable extends { [key: string]: "Url" | "IpAddress" | StringConstructor | BooleanConstructor | NumberConstructor | readonly (string | number | boolean | StringConstructor | BooleanConstructor | NumberConstructor)[] }>(variable: Variable, validator?: (variable: RequiresType<Variable>) => boolean) {
+    const output = Object.keys(variable).reduce((a, c) => ({ ...a, [c]: process.env[c] }), {}) as RequiresType<Variable>
+    if (validator) {
+        const valid = validator(output)
+        if (!valid) {
+            throw new Error("Invalid Environment Variables")
+        }
+    } else {
+        Object.keys(variable).forEach((key, i) => {
+            const validatePrimitive = (key, value, type) => {
+                if (type === String) {
+                    const ok = value === String(value) // ?
+                    if (!ok) {
+                        throw new Error(`Could not safely convert required value ${key} from ${value || "<empty>"} to String.`)
+                    }
+                    return value
+                } else if (type === Boolean) {
+                    const isTrue = ["yes", "true", "on", "y", "1"].indexOf(value?.toLowerCase()) >= 0
+                    const isFalse = ["no", "false", "off", "n", "0"].indexOf(value?.toLowerCase()) >= 0
+                    if (!isTrue && !isFalse) {
+                        throw new Error(`Could not safely convert required value ${key} from ${value || "<empty>"} to Boolean.`)
+                    }
+                    return isTrue
+                } else if (type === Number) {
+                    const ok = !isNaN(parseFloat(value as string | undefined))
+                    if (!ok) {
+                        throw new Error(`Could not safely convert required value ${key} from ${value || "<empty>"} to Number.`)
+                    }
+                    return parseFloat(value as string)
+                } else if (type === Url) {
+                    try {
+                        const url = new URL(value)
+                        return url
+                    } catch (e) {
+                        throw new Error(`Could not safely convert required value ${key} from ${value || "<empty>"} to URL.`)
+                    }
+                } else if (type === IpAddress) {
+                    const ok = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(value)
+                    if (!ok) {
+                        throw new Error(`Could not safely convert required value ${key} from ${value || "<empty>"} to IpAddress.`)
+                    }
+                    return value.toString()
+                } else {
+                    if (typeof type === "string" || typeof type === "boolean" || typeof type === "number") {
+                        const ok = type.toString() === value.toString()
+                        if (!ok) {
+                            throw new Error(`Could not safely convert required value ${key} from ${value || "<empty>"} to ${type}`)
+                        }
+                        return value
+                    }
+                }
+            }
+            const realValue = output[key]
+            const keyType = variable[key]
+            if (Array.isArray(keyType)) {
+                const ok = keyType.map((otherType) => {
+                    try {
+                        return validatePrimitive(key, realValue, otherType)
+                    } catch (e) {
+                        return e
+                    }
+                })
+                if (ok.find(e => !(e instanceof Error))) {
+                    (output as any)[key] = ok.find(e => !(e instanceof Error))
+                } else {
+                    const firstError = ok.find(e => e instanceof Error) as Error | undefined
+                    if (firstError) {
+                        throw firstError
+                    }
+                }
+            } else {
+                (output as any)[key] = validatePrimitive(key, realValue, keyType)
+            }
+
+        })
+    }
+    return output
+}
--- tests/env.test.ts
+++ tests/env.test.ts
@@ -1,109 +1,24 @@
-import { executionAsyncId } from "node:async_hooks"
-import { Schema } from "../env"
+import { IpAddress, Requires, RequiresType, Url } from "../index"
 
 describe('env', () => {
     it('should throw an exception if a required variable is not set', async () => {
 
         Object.assign(process.env, {
+            IP: "1.9.99.9",
             HOSTNAME: "foo.com",
             PASSWORD: "123",
             BOOL: "true",
             TIMEOUT: "1000",
             PORT: 8080,
-            AAA: "0"
+            AAA: "0",
+            ENDPOINT: "https://foo.bar?abc=def"
         })
-
-        type RequiresType<T> = {
-            [P in keyof T]
-            : T[P] extends (...args: any) => any
-            ? ReturnType<T[P]>
-            : T[P] extends readonly (infer U)[]
-            ? (
-                U extends StringConstructor
-                ? string
-                : U extends BooleanConstructor
-                ? boolean
-                : U extends NumberConstructor
-                ? number
-                : U
-            )
-            : T[P] extends infer U
-            ? U
-            : never
-        }
-
-        function Requires<Variable extends { [key: string]: StringConstructor | BooleanConstructor | NumberConstructor | readonly (string | number | boolean | StringConstructor | BooleanConstructor | NumberConstructor)[] }>(variable: Variable, validator?: (variable: RequiresType<Variable>) => boolean) {
-            const output = Object.keys(variable).reduce((a, c) => ({ ...a, [c]: process.env[c] }), {}) as RequiresType<Variable>
-            if (validator) {
-                const valid = validator(output)
-                if (!valid) {
-                    throw new Error("Invalid Environment Variables")
-                }
-            } else {
-                Object.keys(variable).forEach((key, i) => {
-                    const validatePrimitive = (key, value, type) => {
-                        if (type === String) {
-                            const ok = value === String(value) // ?
-                            if (!ok) {
-                                throw new Error(`Could not safely convert required value ${key} from ${value || "<empty>"} to String.`)
-                            }
-                            return value
-                        } else if (type === Boolean) {
-                            const isTrue = ["yes", "true", "on", "y", "1"].indexOf(value?.toLowerCase()) >= 0
-                            const isFalse = ["no", "false", "off", "n", "0"].indexOf(value?.toLowerCase()) >= 0
-                            if (!isTrue && !isFalse) {
-                                throw new Error(`Could not safely convert required value ${key} from ${value || "<empty>"} to Boolean.`)
-                            }
-                            return isTrue
-                        } else if (type === Number) {
-                            const ok = !isNaN(parseFloat(value as string | undefined))
-                            if (!ok) {
-                                throw new Error(`Could not safely convert required value ${key} from ${value || "<empty>"} to Number.`)
-                            }
-                            return parseFloat(value as string)
-                        } else {
-                            if (typeof type === "string" || typeof type === "boolean" || typeof type === "number") {
-                                const ok = type.toString() === value.toString()
-                                if (!ok) {
-                                    throw new Error(`Could not safely convert required value ${key} from ${value || "<empty>"} to ${type}`)
-                                }
-                                return value
-                            }
-                        }
-                    }
-                    const realValue = output[key]
-                    const keyType = variable[key]
-                    if (Array.isArray(keyType)) {
-                        const ok = keyType.map((otherType) => {
-                            try {
-                                return validatePrimitive(key, realValue, otherType)
-                            } catch (e) {
-                                return e
-                            }
-                        })
-                        if (ok.find(e => !(e instanceof Error))) {
-                            (output as any)[key] = ok.find(e => !(e instanceof Error))
-                        } else {
-                            const firstError = ok.find(e => e instanceof Error) as Error | undefined
-                            if (firstError) {
-                                throw firstError
-                            }
-                        }
-                    } else {
-                        (output as any)[key] = validatePrimitive(key, realValue, keyType)
-                    }
-
-                })
-            }
-            return output
-        }
 
         const f = Requires({ DEBUG: String })
 
-
         expect<{ DEBUG: string }>(f)
 
-        function Merge<
+        function TypeEnvy<
             One extends RequiresType<any>,
             Two extends RequiresType<any>,
             Three extends RequiresType<any>,
@@ -117,8 +32,10 @@ describe('env', () => {
             return { ...one, ...two, ...three, ...four } as One & Two & Three & Four
         }
 
-        const a = Merge(
+        const a = TypeEnvy(
             Requires({
+                IP: IpAddress,
+                ENDPOINT: Url,
                 HOSTNAME: String,
                 USERNAME: String,
                 PASSWORD: String,
@@ -135,6 +52,8 @@ describe('env', () => {
         a // ?
 
         const b: typeof a = {
+            IP: "128.0.0.1",
+            ENDPOINT: new URL("ftp://foo.bar"),
             ANOTHER_VAR: false,
             SOME_VAR: "d",
             SOME_PICK: "bar",
@@ -158,6 +77,7 @@ describe('env', () => {
 
 
         new Foo({
+            IP: "1.1.1.1",
             BOOL: true,
             DEBUG: false,
             HOSTNAME: "",
@@ -165,7 +85,8 @@ describe('env', () => {
             PORT: 8080,
             TIMEOUT: 9,
             USERNAME: "",
-            AAA: 1
+            AAA: 1,
+            ENDPOINT: new URL("http://s")
         })
 
     })
